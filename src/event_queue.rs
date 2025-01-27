010a801d5b698004c688f64cc2823e899e4d1523
kratos2377 <shobityadav23@gmail.com>
2025-01-27 22:01:36 +0530

    Added Cross beam support for cache padded pointers

--- src/event_queue.rs
+++ src/event_queue.rs
@@ -1,5 +1,6 @@
-use std::sync::{atomic::{AtomicBool, AtomicU64}, RwLock};
+use std::sync::{atomic::{AtomicBool, AtomicU64, Ordering}, RwLock};
 
+use crossbeam::utils::CachePadded;
 use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::model::GameBetSettleKafkaPayload;
@@ -10,8 +11,8 @@ use crate::model::GameBetSettleKafkaPayload;
 pub struct EventQueue {
 
     pub events_queue: Vec<GameBetSettleKafkaPayload>,
-    pub head: u32,
-    pub tail: u32,
+    pub head: CachePadded<AtomicU64>,
+    pub tail: CachePadded<AtomicU64>,
     pub len: u32,
     pub executors_senders: Vec<Sender<GameBetSettleKafkaPayload>>,
     pub executors_queue: Vec<String>,
@@ -30,8 +31,8 @@ impl EventQueue {
 
         Self {
             events_queue: Vec::with_capacity(6000),
-            head: 0,
-            tail: 0,
+            head: CachePadded::new(AtomicU64::new(0)),
+            tail: CachePadded::new(AtomicU64::new(0)),
             len: 0,
             executors_senders: Vec::with_capacity(20),
             //Change this to a lock free concurrent queue
@@ -46,5 +47,57 @@ impl EventQueue {
 
             self.executors_senders[index] = exec;
     }
+
+
+    pub fn get_len(&self) -> u64 {
+        let head = self.head.load(Ordering::Acquire);
+        let tail = self.tail.load(Ordering::Acquire);
+        let capacity = self.events_queue.capacity() as u64;
+
+        if tail >= head {
+            tail - head
+        } else {
+            (tail + capacity) - head
+        }
+    }
+
+
+    pub fn push(&self, event: GameBetSettleKafkaPayload) -> Result<(), &'static str> {
+        let current_tail = self.tail.load(Ordering::Relaxed);
+        let next_tail = (current_tail + 1) % self.events_queue.capacity() as u64;
+        let current_head = self.head.load(Ordering::Acquire);
+
+        if next_tail == current_head {
+            return Err("Buffer full");
+        }
+
+        unsafe {
+            let slot = self.events_queue.as_ptr().add(current_tail as usize) as *mut _;
+            std::ptr::write(slot, event);
+        }
+
+        self.tail.store(next_tail, Ordering::Release);
+        Ok(())
+    }
+
+    // Pop an event using atomic operations (immutable `&self`)
+    pub fn pop(&self) -> Option<GameBetSettleKafkaPayload> {
+        let current_head = self.head.load(Ordering::Relaxed);
+        let current_tail = self.tail.load(Ordering::Acquire);
+
+        if current_head == current_tail {
+            return None;
+        }
+
+        let event = unsafe {
+            let slot = self.events_queue.as_ptr().add(current_head as usize);
+            std::ptr::read(slot)
+        };
+
+        let next_head = (current_head + 1) % self.events_queue.capacity() as u64;
+        self.head.store(next_head, Ordering::Release);
+        Some(event)
+    }
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::collections::btree_map::Range;
+use std::{collections::{btree_map::Range, HashMap}, net::SocketAddr, sync::{atomic::Ordering, Arc}};
 
-use conf::configuration;
+use axum::{response::IntoResponse, routing::get, Router};
+use conf::{config_types::ServerConfiguration, configuration};
 use event_queue::EventQueue;
 use executor::BetSettleExecutor;
-
+use model::GameBetSettleKafkaPayload;
+use rdkafka::{consumer::StreamConsumer, Message};
+use serde_json::json;
+use tokio::{spawn, sync::Mutex, task::JoinHandle};
+use tracing::{error, warn};
+use crate::configuration::Configuration;
 
 
 pub mod conf;
@@ -13,15 +19,17 @@ pub mod errors;
 pub mod event_queue;
 pub mod model;
 
+pub const START_GAME_SETTLE_EVENT: &str = "start_game_settle_game_event";
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+async fn main()   {
     let hy_config = configuration::Configuration::load().unwrap();
   //  dotenv().ok();
 
   let kafka_consumer = kafka::consumer::init_consumers(&hy_config.kafka).unwrap();
   let kafka_producer = kafka::producer::create_new_kafka_producer(&hy_config.kafka).unwrap();
 
-  let mut event_queue = EventQueue::new();
+  let mut event_queue = Box::leak(Box::new(EventQueue::new()));
 
   for ind in 0..hy_config.executors_config.number_of_executors {
 
@@ -31,6 +39,175 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>  {
     event_queue.add_new_executor(ind.clone(), executor.executor_event_sender);
   }
 
-    Ok(())
+
+
+
+  let game_bet_settlers_handle = init_game_bet_settle_consumers(event_queue, &hy_config, kafka_consumer);
+
+
+  start_web_server(&hy_config.server, vec![game_bet_settlers_handle])
+    .await
+
+
 }
+
+
+
+async fn start_web_server(
+  config: &ServerConfiguration,
+  shutdown_handles: Vec<JoinHandle<()>>,
+) {
+  // Initialize routing
+  let routing = init_routing();
+
+  // Start server
+  let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
+  tracing::info!("listening on {addr}");
+
+  let listener = tokio::net::TcpListener::bind("127.0.0.1:3030")
+  .await
+  .unwrap();
+println!("listening on {}", listener.local_addr().unwrap());
+axum::serve(listener, routing.into_make_service_with_connect_info::<SocketAddr>()).with_graceful_shutdown(shutdown_signal(shutdown_handles)).await.unwrap();
+
+  // Shutdown tracing provider
+}
+
+
+pub async fn shutdown_signal(shutdown_handles: Vec<JoinHandle<()>>) {
+  let ctrl_c = async {
+      tokio::signal::ctrl_c()
+          .await
+          .expect("Initialization of Ctrl+C handler failed");
+  };
+
+  #[cfg(unix)]
+  let terminate = async {
+      tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+          .expect("Initialization of signal handler failed")
+          .recv()
+          .await;
+  };
+
+  #[cfg(not(unix))]
+  let terminate = std::future::pending::<()>();
+
+  tokio::select! {
+      _ = ctrl_c => {},
+      _ = terminate => {},
+  }
+
+  for handle in shutdown_handles {
+      handle.abort();
+  }
+}
+
+
+pub async fn health() -> impl IntoResponse {
+  axum::Json(json!({ "status" : "UP" }))
+}
+
+
+fn init_routing() -> Router {
+  let base_router = Router::new().route("/health", get(health));
+
+  return base_router;
+
+}
+
+
+fn init_game_bet_settle_consumers(
+  event_queue: &'static mut EventQueue,
+  config: &Configuration,
+  kafka_consumers: HashMap<String, StreamConsumer>,
+) -> JoinHandle<()> {
+
+  let mut kafka_joins: Vec<JoinHandle<()>> = vec![];
+
+  for (key_topic , value) in kafka_consumers.into_iter() {
+     let kf_join =  listen(
+        event_queue,
+          config,
+          value,
+          key_topic
+      );
+
+      kafka_joins.push(kf_join);
+  }
+
+  let join_handle = spawn(async move {
+      for handle in kafka_joins {
+          handle.await.unwrap();
+      }
+  });
+
+  return join_handle;
+  
+
+}
+
+
+pub fn listen(
+  event_queue: &'static EventQueue,
+  config: &Configuration,
+  stream_consumer: StreamConsumer,
+  key_topic: String,
+) -> JoinHandle<()> {
+  let topic = key_topic.clone();
+
+  // Start listener
+  tokio::spawn(async move {
+      do_listen( event_queue,&stream_consumer, topic ).await;
+  })
+}
+
+pub async fn do_listen(
+  event_queue: &'static EventQueue,
+  stream_consumer: &StreamConsumer,
+  topic_name: String,
+) {
+
+
+  loop {
+    if event_queue.get_len() < 5000 {
+          match stream_consumer.recv().await {
+            Err(e) => warn!("Error: {}", e),
+            Ok(message) => {
+
+            let topic = message.topic();
+            let payload = String::from_utf8(message.payload().unwrap().to_vec()).unwrap();
+
+            match topic {
+
+              START_GAME_SETTLE_EVENT=> {
+                let game_bet_payload = serde_json::from_str(&payload);
+
+                if game_bet_payload.is_err() {
+                  error!("Some error occured while parsing string to GameBetKafkaPayload")
+                } else {
+                  let game_bet_convertred_record: GameBetSettleKafkaPayload = game_bet_payload.unwrap();
+
+
+                  //later add logic that if push is unsuccessful that publish fail event back
+                 let _ =  event_queue.push(game_bet_convertred_record);
+
+              }
+
+            }
+
+        
+                _ => {
+                    println!("No topics found")
+                  }
+
+            }
+
+                
+        }
+        }
+    }
+}
+
+}
+
 
